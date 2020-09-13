3e3e3507b631bf9d272ce86e743ef07e21e22ccf
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-09-13 18:18:40 +0900

    simple router test

--- src/router/index.js
+++ src/router/index.js
@@ -18,6 +18,14 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/xxx",
+    name: "Test",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ "../views/Test.vue")
   }
 ];
 
---
+++ src/views/Test.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="home">
+    <p>test 영역입니다.</p>
+  </div>
+</template>
+
+<script></script>
